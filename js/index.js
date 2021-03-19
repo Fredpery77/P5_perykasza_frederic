0175993fe18d9eaaa83593c5e012c84c4de348e4
Fredpery77 <fredpery77@msn.com>
2021-03-19 17:33:14 +0100

    modification index.js

--- js/index.js
+++ js/index.js
@@ -1,38 +1,17 @@
 //-----------------------------------récupère les données de l'API---------------------------
-
-// ajouter une donnée dans le local storage
-
-/*localStorage.setItem("nom", "PERY-KASZA");
-localStorage.setItem("prenom", "frederic");
-localStorage.setItem("lieu", "France");
-
-// récupérer une donnée dans le local storage
-
-let maDonnee = localStorage.getItem("prenom");
-console.log(maDonnee);
-
-// supprimer une donnée dans le local storage
-localStorage.removeItem("prenom");
-
-// supprimer toutes les données dans le local storage
- localStorage.clear(); */
-
 const dataApi = fetch('http://localhost:3000/api/teddies');
 
 dataApi.then(async (responseData) => {
 
-
   const response = await responseData.json();
   const section = document.querySelector('.liste-nounours');
   
-
   let name = [];
   let description = [];
   let imageUrl = [];
   let price = [];
   let colors = [];
   let id = [];
-
 
   response.forEach((element, i) => {
     name[i] = element.name;
@@ -47,9 +26,10 @@ dataApi.then(async (responseData) => {
     + '<li>Nom du produit : <span id="productName">' + name[i] + '</span></li>'
     + `<li>Description du Produit : <span id="productDescription">`+ description[i] + `</span></li>`
     + `<li>Prix :<span id="productPrice">` + price[i]/100 + ",00 €" + `</span></li>`
-    + `<a href="./products.html"><button>Voir le produit</button></a>`
+    + `<a href="./products.html?=${id[i]}"><button>Voir le produit</button></a>`
     + '</ul></section>';                               
   });
 });
+
 
 
--- js/product.js
+++ js/product.js
@@ -1,13 +1,13 @@
 //-------------------- Affichage de la page produit -------------------------
 
-const queryString = window.location.search;
+/*const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
-const imageDuProduit = document.querySelector('#productImage');
-const nomDuProduit = document.querySelector("#productName");
-const descriptifDuProduit = document.querySelector("#productDescription");
-const prixDuProduit = document.querySelector("#productPrice");
-const colorsList = document.querySelector("#productColor");
+const imageDuProduit = document.querySelector('.image-du-produit');
+const nomDuProduit = document.querySelector(".image-du-produit");
+const descriptifDuProduit = document.querySelector(".image-du-produit");
+const prixDuProduit = document.querySelector(".image-du-produit");
+const colorsList = document.querySelector(".image-du-produit");
 
 
  const getIP = () => {
@@ -16,18 +16,54 @@ const colorsList = document.querySelector("#productColor");
       .then(res => res.json())
       .then(res => {
 
-        const colorsArray = res.colors;
 
-        nomDuProduit.innerHTML = element.name;
-        imageDuProduit.innerHTML = '<img src="' + element.imageUrl + '"/>';
-        descriptifDuProduit.innerHTML = element.description;
-        prixDuProduit.innerHTML = element.price/100  + ",00 €"; 
-        colorsArray.forEach(element => {
-            colorsList.innerHTML += element;
-            colorsList.innerHTML += '<div' + element + ';"></div>';
+        nomDuProduit.innerHTML = res.name;
+        imageDuProduit.innerHTML = '<img src="' + res.imageUrl + '"/>';
+        descriptifDuProduit.innerHTML = res.description;
+        prixDuProduit.innerHTML = res.price/100  + ",00 €"; 
+        colorsList.innerHTML += res.colors;
+            
         });
-
-    })
 }
-getIP()
+getIP() */
+
+// récupère tous les éléments------------------------
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id')
+
+const dataApi = fetch('http://localhost:3000/api/teddies');
+
+dataApi.then(async (responseData) => {
+
+  const response = await responseData.json();
+  const section = document.querySelector('.image-du-produit');
+
+  let name = [];
+  let description = [];
+  let imageUrl = [];
+  let price = [];
+  let colors = [];
+  let id = [];
+
+  response.forEach((element, i) => {
+    name[i] = element.name;
+    imageUrl[i] = element.imageUrl;
+    description[i] = element.description;
+    price[i] = element.price;
+    colors[i]= element.colors;
+    id [i] = element.id;
+
+    section.innerHTML += '<section id="#affichageduProduit"><ul>'
+    + '<li><span id="productImage"><img class="image-nounours" src="' + imageUrl[i] + '"/> </span></li>'
+    + '<li>Nom du produit : <span id="productName">' + name[i] + '</span></li>'
+    + `<li>Description du Produit : <span id="productDescription">`+ description[i] + `</span></li>`
+    + `<li>Prix :<span id="productPrice">` + price[i]/100 + ",00 €" + `</span></li>`
+    + `<a class="ajoutezAuPanier" href="./panier.html"><button id="btnAjoutezAuPanier">Ajoutez au panier</button></a>`
+    + '</ul></section>';                               
+  });
+});
+
+
 
