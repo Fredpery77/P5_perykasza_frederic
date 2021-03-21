3656ecb87c3943d3fa9f53cbb9b1b9c634b9e113
Fredpery77 <fredpery77@msn.com>
2021-03-21 13:39:33 +0100

    modification fichiers js

--- js/index.js
+++ js/index.js
@@ -19,14 +19,14 @@ dataApi.then(async (responseData) => {
     description[i] = element.description;
     price[i] = element.price;
     colors[i]= element.colors;
-    id [i] = element.id;
+    id[i] = element._id;
      
     section.innerHTML += '<section class="carteNounours"><ul>'
     + '<li><span id="productImage"><img class="image-nounours" src="' + imageUrl[i] + '"/> </span></li>'
     + '<li>Nom du produit : <span id="productName">' + name[i] + '</span></li>'
     + `<li>Description du Produit : <span id="productDescription">`+ description[i] + `</span></li>`
     + `<li>Prix :<span id="productPrice">` + price[i]/100 + ",00 €" + `</span></li>`
-    + `<a href="./products.html?=${id[i]}"><button>Voir le produit</button></a>`
+    + `<a href="./products.html?id=${id[i]}"><button>Voir le produit</button></a>`
     + '</ul></section>';                               
   });
 });
--- js/product.js
+++ js/product.js
@@ -33,36 +33,37 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
 
-const dataApi = fetch('http://localhost:3000/api/teddies');
+const dataApi = fetch('http://localhost:3000/api/teddies/' + id);
 
 dataApi.then(async (responseData) => {
 
   const response = await responseData.json();
   const section = document.querySelector('.image-du-produit');
+  console.log(response);
+  let name = response.name;
+  let description = response.description;
+  let imageUrl = response.imageUrl;
+  let price = response.price;
+  let colors = response.colors;
+  console.log(colors);
+  var html = '';
 
-  let name = [];
-  let description = [];
-  let imageUrl = [];
-  let price = [];
-  let colors = [];
-  let id = [];
+    html += '<section id="#affichageduProduit"><ul>'
+    + '<li><span id="productImage"><img class="image-nounours" src="' + imageUrl + '"/> </span></li>'
+    + '<li>Nom du produit : <span id="productName">' + name + '</span></li>'
+    + `<li>Description du Produit : <span id="productDescription">`+ description + `</span></li>`
+    + `<li>Prix :<span id="productPrice">` + price/100 + ",00 €" + `</span></li>`
+    + `<li>Couleurs : </li></span>`
+    + `<form><select id="productQuantity" name="quantity" id="quantity">`;
 
-  response.forEach((element, i) => {
-    name[i] = element.name;
-    imageUrl[i] = element.imageUrl;
-    description[i] = element.description;
-    price[i] = element.price;
-    colors[i]= element.colors;
-    id [i] = element.id;
+    colors.forEach(function(color){
+      html += `<option value="` + color + `">` + color + `</option>`;
+    });
 
-    section.innerHTML += '<section id="#affichageduProduit"><ul>'
-    + '<li><span id="productImage"><img class="image-nounours" src="' + imageUrl[i] + '"/> </span></li>'
-    + '<li>Nom du produit : <span id="productName">' + name[i] + '</span></li>'
-    + `<li>Description du Produit : <span id="productDescription">`+ description[i] + `</span></li>`
-    + `<li>Prix :<span id="productPrice">` + price[i]/100 + ",00 €" + `</span></li>`
-    + `<a class="ajoutezAuPanier" href="./panier.html"><button id="btnAjoutezAuPanier">Ajoutez au panier</button></a>`
-    + '</ul></section>';                               
-  });
+    html += `</select></form><a class="ajoutezAuPanier" href="./panier.html"><button id="btnAjoutezAuPanier">Ajoutez au panier</button></a>`
+    + '</ul></section>';       
+    
+    section.innerHTML = html;
 });
 
 
